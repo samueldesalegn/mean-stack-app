d329ecd01d197439b092e7581f2ceba73a1f969a
samueldesalegn <samueldesalegnt@gmail.com>
2024-06-20 16:14:31 -0400

    database to ddb

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
+import AWS from 'aws-sdk';
 import express, { json } from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
-import mongoose from 'mongoose';
 import 'dotenv/config';
 import medicationsRouter from './medications/medications.router.js';
 import usersRouter from './users/users.router.js';
@@ -16,22 +16,11 @@ const __dirname = dirname(__filename);
 
 const app = express();
 
-(async function () {
-    try {
-        if (process.env.DB_SERVER_URL) {
-            await mongoose.connect(process.env.DB_SERVER_URL, {
-                useNewUrlParser: true,
-                useUnifiedTopology: true,
-            });
-            console.log('MongoDB server is connected successfully');
-        } else {
-            throw new Error('DB server URL is not found');
-        }
-    } catch (error) {
-        console.error('Error connecting to MongoDB:', error);
-        process.exit(1);
-    }
-})();
+AWS.config.update({
+    region: 'us-east-1',
+});
+
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
 app.use(cors());
 app.use(morgan('dev'));
@@ -45,12 +34,11 @@ app.use('/users', usersRouter);
 app.use('/medications', medicationsRouter);
 
 // Catch all unhandled requests
-app.all('*', (req, res, next) => next(new ErrorResponse('Route not found', 404)));
+app.all('*', async (req, res, next) => next(new ErrorResponse('Route not found', 404)));
 
 // Error handler
 app.use((error, req, res, next) => {
-    res.status(error.status || 500).json({ success: false, message: error.message });
+    res.status(error.status || 500).json({ success: false, data: error.message });
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Express server is listening on port ${PORT}`));
+app.listen(process.env.PORT, () => console.log(`Express server is listening on port ${process.env.PORT}`));
